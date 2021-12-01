fdb22d391c36080c9168b90f4c8934814f35a379
Bertie Blackman <blackmanrgh@gmail.com>
2021-12-01 11:53:05 +0000

    Day 1 part 2

--- src/one/one.ts
+++ src/one/one.ts
@@ -5,10 +5,18 @@ const filename = './src/one/input.txt'
   const fileInput = await fs.readFile(filename, 'utf8')
   const arrayOfDepths = fileInput.split('\n').map((depth) => parseInt(depth))
 
-  const total = arrayOfDepths.reduce<number>((tally, currentDepth, index) => {
-    if (index !== 0 && currentDepth > arrayOfDepths[index - 1]) {
+  const arrayOfDepthsGrouped = arrayOfDepths.reduce<number[]>((groupedArray, currentDepth, index) => {
+    if (index > 1) {
+      return [...groupedArray, currentDepth + arrayOfDepths[index - 1] + arrayOfDepths[index - 2]]
+    }
+    return groupedArray
+  }, [])
+
+  const total = arrayOfDepthsGrouped.reduce<number>((tally, currentDepth, index) => {
+    if (index !== 0 && currentDepth > arrayOfDepthsGrouped[index - 1]) {
       return tally + 1
     }
+
     return tally
   }, 0)
 
