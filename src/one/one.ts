424ce58dbabf6d3f8dd43adfb2e22eca529e21cf
Bertie Blackman <blackmanrgh@gmail.com>
2021-12-01 11:32:47 +0000

    Day 1 part 1

--- src/one/one.ts
+++ src/one/one.ts
@@ -5,12 +5,12 @@ const filename = './src/one/input.txt'
   const fileInput = await fs.readFile(filename, 'utf8')
   const arrayOfDepths = fileInput.split('\n')
 
-  const tally = arrayOfDepths.reduce<number>((tally, currentDepth, index) => {
+  const total = arrayOfDepths.reduce<number>((tally, currentDepth, index) => {
     if (index !== 0 && parseInt(currentDepth) > parseInt(arrayOfDepths[index - 1])) {
       return tally + 1
     }
     return tally
   }, 0)
 
-  console.log(tally)
+  console.log(total)
 })()
