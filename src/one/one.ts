9f91fda416141a285d9f0e9a6d2421e5eea90215
Bertie Blackman <blackmanrgh@gmail.com>
2021-12-01 11:32:13 +0000

    Day 1 part 1

--- src/one/one.ts
+++ src/one/one.ts
@@ -5,13 +5,12 @@ const filename = './src/one/input.txt'
   const fileInput = await fs.readFile(filename, 'utf8')
   const arrayOfDepths = fileInput.split('\n')
 
-  let tallyOfDepthsHigherThanPrevious = 0
-
-  arrayOfDepths.forEach((depth, index) => {
-    if (index !== 0 && parseInt(depth) > parseInt(arrayOfDepths[index - 1])) {
-      tallyOfDepthsHigherThanPrevious += 1
+  const tally = arrayOfDepths.reduce<number>((tally, currentDepth, index) => {
+    if (index !== 0 && parseInt(currentDepth) > parseInt(arrayOfDepths[index - 1])) {
+      return tally + 1
     }
-  })
+    return tally
+  }, 0)
 
-  console.log(tallyOfDepthsHigherThanPrevious)
+  console.log(tally)
 })()
