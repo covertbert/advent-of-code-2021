a2d43584efec58547a328adb34dc7e6c6437063f
Bertie Blackman <blackmanrgh@gmail.com>
2021-12-03 13:55:28 +0000

    Day 2 part 2

--- src/two/two.ts
+++ src/two/two.ts
@@ -1,27 +1,32 @@
 import { promises as fs } from 'fs'
 const filename = './src/two/input.txt'
 
-type Coordinates = { horizontal: number; depth: number }
+type Coordinates = { horizontal: number; depth: number; aim: number }
+type Direction = 'forward' | 'up' | 'down'
 ;(async () => {
   const fileInput = await fs.readFile(filename, 'utf8')
   const arrayOfInputs = fileInput.split('\n')
 
   const coordinates: Coordinates = arrayOfInputs.reduce<Coordinates>(
     (finalCoordinates, currentInput) => {
-      const [direction, distance] = currentInput.split(' ')
+      const [direction, distance] = currentInput.split(' ') as [Direction, string]
 
       return {
         horizontal:
-          direction === 'forward' ? (finalCoordinates.horizontal += parseInt(distance)) : finalCoordinates.horizontal,
+          direction === 'forward' ? finalCoordinates.horizontal + parseInt(distance) : finalCoordinates.horizontal,
         depth:
-          direction === 'up'
-            ? (finalCoordinates.depth -= parseInt(distance))
-            : direction === 'down'
-            ? (finalCoordinates.depth += parseInt(distance))
+          direction === 'forward'
+            ? finalCoordinates.depth + finalCoordinates.aim * parseInt(distance)
             : finalCoordinates.depth,
+        aim:
+          direction === 'up'
+            ? finalCoordinates.aim - parseInt(distance)
+            : direction === 'down'
+            ? finalCoordinates.aim + parseInt(distance)
+            : finalCoordinates.aim,
       }
     },
-    { horizontal: 0, depth: 0 },
+    { horizontal: 0, depth: 0, aim: 0 },
   )
 
   console.log(coordinates)
